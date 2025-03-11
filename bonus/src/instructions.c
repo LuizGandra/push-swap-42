363b6ac81e45d32cca403d106ab010adf4cea0d3
LuizGandra <luizc.gandra29@gmail.com>
2025-03-11 16:48:08 -0300

    Fix bonus bugs

--- bonus/src/instructions.c
+++ bonus/src/instructions.c
@@ -6,7 +6,7 @@
 /*   By: lcosta-g <lcosta-g@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:06:09 by lcosta-g          #+#    #+#             */
-/*   Updated: 2025/03/01 13:32:45 by lcosta-g         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:36:47 by lcosta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,27 +24,27 @@ char	*read_instruction(void)
 
 int	run_instruction(char *instruction)
 {
-	if (!ft_strncmp(instruction, "sa", 2))
+	if (!ft_strcmp(instruction, "sa\n"))
 		sa();
-	else if (!ft_strncmp(instruction, "sb", 2))
+	else if (!ft_strcmp(instruction, "sb\n"))
 		sb();
-	else if (!ft_strncmp(instruction, "ss", 2))
+	else if (!ft_strcmp(instruction, "ss\n"))
 		ss();
-	else if (!ft_strncmp(instruction, "pa", 2))
+	else if (!ft_strcmp(instruction, "pa\n"))
 		pa();
-	else if (!ft_strncmp(instruction, "pb", 2))
+	else if (!ft_strcmp(instruction, "pb\n"))
 		pb();
-	else if (!ft_strncmp(instruction, "ra", 2))
+	else if (!ft_strcmp(instruction, "ra\n"))
 		ra();
-	else if (!ft_strncmp(instruction, "rb", 2))
+	else if (!ft_strcmp(instruction, "rb\n"))
 		rb();
-	else if (!ft_strncmp(instruction, "rr", 2))
+	else if (!ft_strcmp(instruction, "rr\n"))
 		rr();
-	else if (!ft_strncmp(instruction, "rra", 3))
+	else if (!ft_strcmp(instruction, "rra\n"))
 		rra();
-	else if (!ft_strncmp(instruction, "rrb", 3))
+	else if (!ft_strcmp(instruction, "rrb\n"))
 		rrb();
-	else if (!ft_strncmp(instruction, "rrr", 3))
+	else if (!ft_strcmp(instruction, "rrr\n"))
 		rrr();
 	else
 		return (0);
